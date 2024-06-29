028a28a1f79ea7761766703f685f83784308a3d1
Raveen De Silva <117538343+RaveenDE@users.noreply.github.com>
2024-06-29 13:18:48 +0530

    FIrst commit

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  
+
+  return (
+    <Router>
+      <div className='app-container'>
+        <Routes>
+        <Route path="/" element={<h1>Dashboard</h1>}/>
+        <Route path="auth" element={<h1>Sign-in</h1>}/>
+        
+        </Routes>
+        
+
+      </div>
+    </Router>
+  )
+}
+
+export default App
