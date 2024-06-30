b232580e7c2ecc655c9e1815fe71487ba5433b34
Raveen De Silva <117538343+RaveenDE@users.noreply.github.com>
2024-06-30 11:17:25 +0530

    gitignore update

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,18 +2,23 @@ import express, { Express } from "express";
 import mongoose from "mongoose";
 import financialRecordRouter from "./routes/financial-records";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
+const mongoUri = process.env.MONGO_URI;
+
+if (!mongoUri) {
+  throw new Error("MONGO_URI is not defined in the environment variables");
+}
 
 app.use(express.json());
 app.use(cors());
 
-const mongoURI: string =
-  "mongodb+srv://machadop1407:UvOUTpZW7EH85MZl@personalfinancetracker.qb0edtk.mongodb.net/";
-
 mongoose
-  .connect(mongoURI)
+  .connect(mongoUri)
   .then(() => console.log("CONNECTED TO MONGODB!"))
   .catch((err) => console.error("Failed to Connect to MongoDB:", err));
 
